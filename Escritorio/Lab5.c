909b5a48701e87fb0b88071186fcc146835fcc02
VictorRoman <vjr823@msn.com>
2014-02-12 00:07:38 -0500

    Update Lab5.c
    Programa base listo

--- Escritorio/Lab5.c
+++ Escritorio/Lab5.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+#include <pthread.h>
 
 //Prototipo de funciones
 void transferencias(int cuentas[]);
 
 //Variables globales
-int numHilos;
-int tiempo;
-int numCuentas;
-int valorIni;
+int numHilos;//Guarda el numero de hilos pasado por el parametro
+int tiempo;//Guarda la cantidad de tiempo a correr pasado por el parametro
+int numCuentas;//Guarda el numero de cuentas pasado por el parametro
+int valorIni;//Guarda el valor inicial pasado por el parametro
+pthread_mutex_t mutex;//mutex para sincronizar la operacion sobre los datos
 
 int main(int argc, char* argv[])
 {
@@ -52,38 +55,61 @@ int main(int argc, char* argv[])
 	{//ciclo para guardar el valor inicial
 		cuentas[i] = (int)valorIni;
 	}
-	transferencias(cuentas);
-	for(i = 0; i < numCuentas; i++)
-	{
-	int j = (int)cuentas[i];
-		printf("cuenta %d: %d\n", i, j);
+
+	pthread_t hilo[numHilos];//Creo la cantidad de hilos
+
+	pthread_mutex_init(&mutex, NULL);//inicio el mutex
+	
+	for(i = 0; i < numHilos; i++)
+	{//ciclo para ejecutar el metodo transacciones de todos los hilos
+		pthread_create(&hilo[i], NULL, (void*)transferencias, cuentas);
 	}
+
+	pthread_mutex_destroy(&mutex);//destruye el mutex
+
+	for(i = 0; i < numHilos; i++)
+	{//ciclo para esperar a todos los hilos
+		pthread_join(hilo[i], NULL);
+	}
+
+	int suma = 0;//variable para obtener la suma de las cuentas
+	for(i = 0; i < numCuentas; i++)
+	{//cilo para realizar la suma de las cuentas y mostrar la cantidad en cada cuenta
+		int j = (int)cuentas[i];
+		printf("cuenta %d %d\n", i, j);
+		suma = suma + j;
+	}
+	printf("suma %d\n", suma);//muestra la suma en todas las cuentas
 	return EXIT_SUCCESS;
 }
 
 void transferencias(int cuentas[])
 {//metodo para realizar transferencias
-	srand(time(NULL));//genera un numero aleatorio para la cuenta
-	int numero1 = rand() % (numCuentas);//guarda el numero aleatorio
-	int numero2;
-	do
-	{
-		srand(time(NULL));//genera otro numero aleatorio para la cuenta
-		numero2 = rand() % (numCuentas);//guarda el otro numero
-	}while(numero1 == numero2);//verifica que no salga el mismo numero
-	printf("num1: %d\n", numero1);
-	printf("num2: %d\n", numero2);
-	srand(time(NULL));//genera otro numero aleatorio para el valor transferido
-	int cantidad = rand() % (valorIni);//guarda el numero
-	printf("cant: %d\n", cantidad);
-	if((cuentas[numero1] - cantidad) >= 0)
-	{//verifica que la cuenta no quede menor que cero
-	//se hace la transferencia
-		cuentas[numero1] = cuentas[numero1] - cantidad;
-		cuentas[numero2] = cuentas[numero2] + cantidad;
+	pthread_mutex_lock(&mutex);//bloquea el mutex para una entrada sincronizada
+	int i;
+	for(i = 0; i < ((tiempo * 1000) / 1000); i++)
+	{//ciclo para que cada el hilo se ejecute el tiempo pasado por parametro
+		srand((time(NULL) * pthread_self()));//genera un numero aleatorio para la cuenta
+		int numero1 = rand() % (numCuentas);//guarda el numero aleatorio
+		int numero2;
+		do
+		{
+			srand((time(NULL) * pthread_self()));//genera otro numero aleatorio para la cuenta
+			numero2 = rand() % (numCuentas);//guarda el otro numero
+		}while(numero1 == numero2);//verifica que no salga el mismo numero
+		srand((time(NULL) * pthread_self()));//genera otro numero aleatorio para el valor transferido
+		int cantidad = rand() % (valorIni);//guarda el numero
+		if((cuentas[numero1] - cantidad) >= 0)
+		{//verifica que la cuenta no quede menor que cero
+		//se hace la transferencia
+			cuentas[numero1] = cuentas[numero1] - cantidad;
+			cuentas[numero2] = cuentas[numero2] + cantidad;
+		}
+		else
+		{//con este mensaje se verifica que los hilos no traten de acceder a la misma cuenta siempre
+			printf("No se puede realizar la transferencia de la cuenta %d a la cuenta %d ya que no se cuenta con los fondos suficientes\n", numero1, numero2);
+		}
+	sleep(1);//duerme un segundo
 	}
-	else
-	{
-		printf("No se puede realizar la transferencia ya que no se cuenta con los suficientes fondos");
-	}
+	pthread_mutex_unlock(&mutex);//desbloquea el mutex
 }
